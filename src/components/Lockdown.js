0a8ab36021dcd98478784a37610a26d9fabde071
snappyyw <snippymr@yandex.ru>
2021-06-06 13:28:01 +0300

    2

--- src/components/Lockdown.js
+++ src/components/Lockdown.js
@@ -1,9 +1,17 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 function Lockdown() {
   return (
-    <div>
-      <p>Lockdown</p>
+    <div className={"body"}>
+      <h1 className="title">Приложение наблюдения с блокированием экрана</h1>
+      <ReactPlayer
+        height="500px"
+        width="100%"
+        controls="true"
+        url="https://www.youtube.com/watch?v=nA7ju_H5aac"
+      />
+      <h3 className={"subtitle"}>Материал к видео:</h3>
     </div>
   );
 }
